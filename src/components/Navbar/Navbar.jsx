5f6c21717fb43da48d4ee9b281329195e810ab77
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-16 12:10:37 +0530

    added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import {
+  AppBar,
+  Box,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <AppBar position="sticky" sx={{ background: "black" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            // onClick={goToHome}
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            The Generics
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              //   onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Provider } from "react-redux";
-import store from "./reducers/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ToastContainer position="top-right" autoClose={3000} theme="light" />
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+      <ToastContainer position="top-right" autoClose={3000} theme="light" />
+    </BrowserRouter>
   </React.StrictMode>
 );
